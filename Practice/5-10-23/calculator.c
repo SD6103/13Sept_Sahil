55f47c233922fa796de0615f8836f70665251468
sahil_61 <99475933+Sahil6103@users.noreply.github.com>
2023-10-07 16:37:12 +0530

    Update calculator.c

--- Practice/5-10-23/calculator.c
+++ Practice/5-10-23/calculator.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 void main()
 {
-    int a, b;
+    int a, b; //variable
     printf("Enter value of a: ");
-    scanf("%d", &a);
+    scanf("%d", &a); // get value from user
     printf("Enter value of b: ");
-    scanf("%d", &b);
-    printf("\nAddition is: %d", a + b);
-    printf("\nSubtraction is: %d", a - b);
-    printf("\nMultiplication is: %d", a * b);
-    printf("\nDivision is: %.1f", (float)a / (float)b);
-    printf("\nModulo is: %d", a % b);
+    scanf("%d", &b); // get value from user
+    printf("\nAddition is: %d", a + b); // addition
+    printf("\nSubtraction is: %d", a - b); //subtration
+    printf("\nMultiplication is: %d", a * b); //multiplication
+    printf("\nDivision is: %.1f", (float)a / (float)b); // division
+    printf("\nModulo is: %d", a % b); // modulo
 }
