0453b093f3c013a13a50e84278b854ddfd86a79d
unknown <sahildhandhukiya0@gmail.com>
2023-12-22 11:33:20 +0530

    21-Dec

--- Assignment/Module - 4.4 (Inh.,Poly.)/mathematic_op.cpp
+++ Assignment/Module - 4.4 (Inh.,Poly.)/mathematic_op.cpp
@@ -6,40 +6,22 @@ using namespace std; // library file
 class math_op // class creation
 {
 public:
-    int addition(int a, int b) // function creation with int peremeter
+    int addition(int a, int b, int c) // function creation with int peremeter
     {
-        return a + b;
-    }
-
-    int addition(float a, float b) // function creation with float peremeter
-    {
-        return a + b;
+        return a + b + c;
     }
 
     int subtraction(int a, int b) // function creation with int peremeter
     {
         return a - b;
     }
-    int subtraction(float a, float b) // function creation with float peremeter
+
+    int multiplication(int a, int b, int c) // function creation with int peremeter
     {
-        return a - b;
+        return a * b * c;
     }
 
-    int multiplication(int a, int b) // function creation with int peremeter
-    {
-        return a * b;
-    }
-
-    int multiplication(float a, float b) // function creation with float peremeter
-    {
-        return a * b;
-    }
-
-    int division(int a, int b) // function creation with int peremeter
-    {
-        return a / b;
-    }
-    int division(float a, float b) // function creation with float peremeter
+    int division(float a, float b) // function creation with int peremeter
     {
         return a / b;
     }
@@ -47,28 +29,22 @@ public:
 
 int main()
 {
-    float n1, n2; // variable
+    float n1, n2, n3; // variable
     cout << "Enter number 1 : ";
     cin >> n1;
     cout << "Enter number 2 : ";
     cin >> n2;
+    cout << "Enter number 3 : ";
+    cin >> n3;
     math_op math; // object
     // function calling
     cout << endl
-         << "addition of int : " << math.addition(n1, n2);
+         << "addition : " << math.addition(n1, n2, n3);
     cout << endl
-         << "addition of float : " << math.addition(n1, n2);
+         << "subtraction : " << math.subtraction(n1, n2);
     cout << endl
-         << "subtraction of int : " << math.subtraction(n1, n2);
+         << "multiplication : " << math.multiplication(n1, n2, n3);
     cout << endl
-         << "subtraction of float : " << math.subtraction(n1, n2);
-    cout << endl
-         << "multiplication of int : " << math.multiplication(n1, n2);
-    cout << endl
-         << "multiplication of float : " << math.multiplication(n1, n2);
-    cout << endl
-         << "division of int : " << math.division(n1, n2);
-    cout << endl
-         << "division of float : " << math.division(n1, n2);
+         << "division : " << math.division(n1, n2);
     return 0;
 }
