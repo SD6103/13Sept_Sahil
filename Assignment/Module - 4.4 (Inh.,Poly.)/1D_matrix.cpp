e83dcb0fe328caa6afb0b31383cb6de6ffaac3ab
unknown <sahildhandhukiya0@gmail.com>
2024-01-06 15:17:23 +0530

    6-Jan

--- Assignment/Module - 4.4 (Inh.,Poly.)/1D_matrix.cpp
+++ Assignment/Module - 4.4 (Inh.,Poly.)/1D_matrix.cpp
@@ -1,10 +1,48 @@
 // Write a Program of Two 1D Matrix Addition using Operator Overloading
 
 #include <iostream>
-using namespace std;
+#include <string.h>
+using namespace std; // library file
 
-class add_matrix
+class matrix // creation of class
 {
-    int arr1[5] = {1, 1, 1, 1, 1};
-    int arr2[5] = {1, 1, 1, 1, 1};
+public:
+    // data member
+    int s1[5];
+    int i;
+
+    // member function
+    void get()
+    {
+        cout << "Enter array elements : ";
+        for (i = 1; i <= 5; i++)
+        {
+            cin >> s1[i];
+        }
+    }
+
+    // operator overloading
+    matrix operator+(matrix &m2)
+    {
+        matrix m3;
+        int add[5];
+        for (int i = 1; i <= 5; i++)
+        {
+            add[i] = s1[i] + m2.s1[i];
+        }
+
+        cout << "Addition : ";
+        for (int i = 1; i <= 5; i++)
+        {
+            cout << " " << add[i];
+        }
+    }
+};
+
+int main()
+{
+    matrix M1, M2, M3; // object creation
+    M1.get();          // function call
+    M2.get();          // function call
+    M3 = M1 + M2;      // addtion through objects
 }
--- Assignment/Module - 4.4 (Inh.,Poly.)/concat_string.cpp
+++ Assignment/Module - 4.4 (Inh.,Poly.)/concat_string.cpp
@@ -1,1 +1,40 @@
 // Write a program to concatenate the two strings using Operator Overloading
+
+#include <iostream>
+#include <string.h>
+
+using namespace std; // library file
+
+class concat
+{
+public:
+    // data member
+    char a[20];
+    // member function
+    void get(char str[20])
+    {
+        strcpy(a, str);
+    }
+
+    // operator overloading
+    concat operator+(concat &c2)
+    {
+        concat c3;
+        cout << endl
+             << "Concatination : " << a << c2.a;
+        return c3;
+    }
+};
+
+int main()
+{
+    char s1[20], s2[20]; // variable
+    cout << "Enter string 1 : ";
+    cin >> s1;
+    cout << "Enter string 2 : ";
+    cin >> s2;
+    concat obj1, obj2, obj3; // object creation
+    obj1.get(s1);            // function call
+    obj2.get(s2);            // function call
+    obj3 = obj1 + obj2;      // concatination through objects
+}
